9e24433fb94969484aa44d8aadd6427f84d8fc10
[GitCraftAmit] <[amitkumar2k00@gmail.com]>
2024-04-10 09:51:18 +0530

    initial commit (files added)

---
+++ src/components/Login.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import Header from "./Header";
+
+const Login = () => {
+  return (
+    <div>
+      <Header />
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/6cefb2f5-90be-4f57-adc4-f6c3c579273d/3943990c-f4e0-4147-82ad-f531e2b547f3/IN-en-20240401-popsignuptwoweeks-perspective_alpha_website_small.jpg"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Login;
